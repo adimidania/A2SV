329b71373f02aa814902089006082ea2ae80e02b
dxnette <59611591+adimidania@users.noreply.github.com>
2024-05-15 22:08:29 +0100

    Time: 103 ms (88.40%) | Memory: 19 MB (72.85%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = defaultdict(int)
-        for i in range(len(nums)):
-            count[nums[i]] += 1
-
-        for num, i in count.items():
-            if i == 1:
-                return num
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        return xor
         
