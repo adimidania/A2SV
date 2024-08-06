1a11f0440f34a445c693db255ce3ad3fa7806f41
Alaa Dania Adimi <59611591+adimidania@users.noreply.github.com>
2024-08-06 20:17:30 +0100

    Time: 40 ms (18.52%) | Memory: 16.3 MB (99.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        memo = defaultdict(int)
+    def climbStairs(self, n: int, memo=None) -> int:
+        if memo is None:
+            memo = {}
         
-        def climb(n):
-            if n == 1:
-                return 1
-            elif n == 2:
-                return 2
-            if n not in memo:
-                memo[n] = climb(n-1) + climb(n-2)
-            return memo[n]
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
         
-        return climb(n)
+        if n not in memo:
+            memo[n] = self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo)
+        
+        return memo[n]
+    
