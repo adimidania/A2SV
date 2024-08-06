7577bf18dc811f664fb54835dc7e2e4a1354ab92
Alaa Dania Adimi <59611591+adimidania@users.noreply.github.com>
2024-08-06 19:05:48 +0100

    Time: 35 ms (70.37%) | Memory: 16.5 MB (51.77%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,10 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        memo = defaultdict(int)
-        if n == 0 or n == 1:
-            return n
+    def fib(self, n: int, memo = {}) -> int:
+        if n in memo:
+            return memo[n]
+        if n > 1:
+            memo[n] = self.fib(n-1, memo) + self.fib(n-2, memo)
+            return memo[n]
         else:
-            if n not in memo:
-                memo[n] = self.fib(n-1) + self.fib(n-2)
-        return memo[n]
+            return n
         
