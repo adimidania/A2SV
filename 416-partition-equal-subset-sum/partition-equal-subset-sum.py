97787b380632acbea173f12e34ee3cf5913591c3
Alaa Dania Adimi <59611591+adimidania@users.noreply.github.com>
2024-08-08 18:43:52 +0100

    Time: 2426 ms (7.31%) | Memory: 652 MB (5.28%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        target = sum(nums)/2
+        n = len(nums)
+        @cache
+        def solve(index, currentSum):
+            if index >= n or currentSum > target:
+                return False
+            if currentSum == target:
+                return True
+            return solve(index + 1, currentSum + nums[index]) or solve(index + 1, currentSum)
+        
+        return solve(0, 0)
+        
