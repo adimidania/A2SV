61b08eb00e892d78fe0efe566875833396174580
Alaa Dania Adimi <59611591+adimidania@users.noreply.github.com>
2024-10-03 19:20:39 +0100

    Time: 191 ms (28.24%) | Memory: 17.4 MB (50.94%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,35 @@
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x: int, y: int) -> None:
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = UnionFind(n)  
+
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    uf.union(i, j) 
+
+        distinct = set(uf.find(i) for i in range(n)) 
+        return len(distinct)
