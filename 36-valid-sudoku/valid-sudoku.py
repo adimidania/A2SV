9c0247ee647f17379ef32b1005368c27660998d7
Alaa Dania Adimi <59611591+adimidania@users.noreply.github.com>
2024-10-07 21:14:11 +0100

    Time: 85 ms (89.17%) | Memory: 16.6 MB (18.42%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -2,7 +2,6 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         n = len(board)
         for row in board:
-            print("rows")
             nums = []
             for num in row:
                 if num != ".":
@@ -11,7 +10,6 @@ class Solution:
                 return False
 
         for i in range(n):
-            print("columns")
             nums = []
             for j in range(n):
                 if board[j][i] != ".":
@@ -20,7 +18,6 @@ class Solution:
                 return False
 
         for i in range(3):
-            print("boxes")
             for j in range(3):
                 nums = []
                 for k in range(3):
